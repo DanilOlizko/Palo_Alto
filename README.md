ca8fe3344131ea5f8cc1cc17a18105e347f7b939
Danil Olizko <olizko.danil@gmail.com>
2017-12-18 15:19:25 +0700

    adaptive

--- README.md
+++ README.md
@@ -2,33 +2,33 @@
 
 
 ####  1. Install nvm:
-'''sh
+```sh
 $ sudo apt-get install build-essential libssl-dev
 $ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 $ source ~/.profile
-'''
+```
 #### 2. Install correct version and check it:
-'''sh
+```sh
 $ nvm install v8.9.1
 $ node-v
-'''
+```
 #### 3. Clone my repository:
-'''sh
+```sh
 $ git clone https://github.com/DanilOlizko/Palo_Alto
-'''
+```
 #### 4.Go to project's folder:
-'''sh
+```sh
 $ cd Palo_Alto
-'''
+```
 #### 5. Install dependencies:
-'''sh 
+```sh 
 $ npm install
-'''
+```
 #### 6. Install gulp globally:
-'''sh 
+```sh 
 $ npm install --global gulp-cli
-'''
+```
 #### 7.Run project:
-'''sh
+```sh
 $ gulp
-'''
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,61 @@
 var gulp = require('gulp');
-var nunjucks = require('gulp-nunjucks');
-var concat = require('gulp-concat');
+var handlebars=require('gulp-compile-handlebars');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var cssmin = require('gulp-cssmin');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 
 
 var path = {
-    css:  'src/styles/*.scss',
-    html: 'src/templates/*.html',
+    css:  './src/*.scss',
+    html: {
+    pages: './src/pages/**/*.hbs',
+    partials: './src/partials/'
+    },
     dist: {
-      css:  'dist/styles/',
-      html: 'dist/'
-    }
+     css:  './dist/',
+     html: './dist/'
+    },
+    watch: {
+     css: './src/**/*.scss',
+     html: './src/**/*.hbs'
+      }
 };
-
-gulp.task('default', function() {
-    gulp.src('src/**/*.css')
-        .pipe(concat('all.css'))
-        .pipe(cssmin())
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('default', function() {
-    gulp.src('src/**/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(concat('all.css'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('default', function() {
-    gulp.src('src/**/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(autoprefixer())
-        .pipe(concat('all.css'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'))
-});
 
 gulp.task('default', ['build', 'serve', 'watch']);
 
 gulp.task('css', function () {
   return gulp.src(path.css)
     .pipe(sass().on('error', sass.logError))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest(path.dist.css));
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(concat('style.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
 });
 
 gulp.task('html', function () {
-  return gulp.src(path.html)
-    .pipe(nunjucks.compile())
-    .pipe(gulp.dest(path.dist.html));
+    return gulp.src(path.html.pages)
+        .pipe(handlebars({}, {
+            ignorePartials: true,
+            batch: [path.html.partials]
+        }))
+        .pipe(rename({
+            dirname: '.',
+            extname: '.html'
+        }))
+        .pipe(gulp.dest(path.dist.html));
 });
 
 gulp.task('build', ['html', 'css']);
 
 gulp.task('watch', function () {
-  gulp.watch(path.css, ['css']);
-  gulp.watch(path.html, ['html']);
+  gulp.watch(path.watch.css, ['css']);
+  gulp.watch(path.watch.html, ['html'])
 });
 
 gulp.task('serve', ['watch'], function() {
@@ -70,4 +65,5 @@ gulp.task('serve', ['watch'], function() {
     }
   });
   gulp.watch('dist/**').on('change', browserSync.reload);
+  });
 
